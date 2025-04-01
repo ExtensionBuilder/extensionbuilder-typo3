bf9e656dd549e5aae889aae222e21bf5ff5173e0
StephanSellner <13609111+StephanSellner@users.noreply.github.com>
2025-04-02 00:21:07 +0200

    Update 0.5.151

--- Resources/Public/JavaScript/Backend/table.js
+++
@@ -1,6 +0,0 @@
-import LiteGraph       from "@extensionbuildertypo3/litegraph/litegraph.js"
-import LiteGraphEditor from "@extensionbuildertypo3/litegraph/litegraph-editor.js"
-
-console.log("Hello world! - table.js");
-
-
